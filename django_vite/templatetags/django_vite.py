d45181f68f1255ea3f73e8e0bf00ba4736d16e26
Jacob Burch-Hill <jacob@burch-hill.com>
2023-04-20 12:02:25 -0500

    how often can i typo?

--- django_vite/templatetags/django_vite.py
+++ django_vite/templatetags/django_vite.py
@@ -132,7 +132,7 @@ class DjangoViteAssetLoader:
         tags.extend(self._generate_css_files_of_asset(path, []))
 
         # Add the script by itself
-        if manifest_entry["file"].endwiths(".css"):
+        if manifest_entry["file"].endswith(".css"):
             tags.append(
                 DjangoViteAssetLoader._generate_css_files_of_asset(
                     urljoin(DJANGO_VITE_STATIC_URL, manifest_entry["file"]),
